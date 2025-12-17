0193c1af2ebd1148bc5529cf78c3bc2a98cbc387
Asmatullah Atal <asmatullah6090@gmail.com>
2025-12-17 12:21:40 -0800

    this is the  the code where the modal is closing and opening through the js code

---
+++ .history/script_20251217102356.js
@@ -0,0 +1,6 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].textContent.a;
---
+++ .history/script_20251217113002.js
@@ -0,0 +1,19 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+this is the awesome this is hte aweosme hti sis hte awesome this is the aweome this i the awesome hi is hte aweosme ht
